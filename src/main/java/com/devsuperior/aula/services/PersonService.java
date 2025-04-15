eb857654003ee6600d60b4cd3a0b9f3468f281ee
Matheus Farias <matheusaf.contato@gmail.com>
2025-04-15 05:26:54 -0300

    Save PersonDTO

--- src/main/java/com/devsuperior/aula/services/PersonService.java
+++ src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.aula.services;
 
+import com.devsuperior.aula.dto.PersonDTO;
 import com.devsuperior.aula.dto.PersonDepartmentDTO;
 import com.devsuperior.aula.entities.Department;
 import com.devsuperior.aula.entities.Person;
@@ -23,13 +24,24 @@ public class PersonService {
         Person entity = new Person();
         entity.setName(dto.getName());
         entity.setSalary(dto.getSalary());
-
         Department dept = departmentRepository.getReferenceById(dto.getDepartment().getId());
-
         // Department dept = new Department();
         //dept.setId(dto.getDepartment().getId());
         entity.setDepartment(dept);
         entity = repository.save(entity);
         return new PersonDepartmentDTO(entity);
     }
+
+    @Transactional
+    public PersonDTO insert(PersonDTO dto){
+        Person entity = new Person();
+        entity.setName(dto.getName());
+        entity.setSalary(dto.getSalary());
+        Department dept = departmentRepository.getReferenceById(dto.getDepartmentId());
+        entity.setDepartment(dept);
+        entity = repository.save(entity);
+        return new PersonDTO(entity);
+    }
+
+
 }
